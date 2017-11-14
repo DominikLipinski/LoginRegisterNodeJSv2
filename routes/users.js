b86c729921c97aad163f7fec0197c26b9c5b4090
Dominik Lipinski <maboonga@gmail.com>
2017-11-14 18:26:55 +0100

    register - post

--- routes/users.js
+++ routes/users.js
@@ -1,14 +1,26 @@
 var express = require('express');
 var router  = express.Router();
 
-//register
+// register (view)
 router.get('/register', function(req, res) {
     res.render('register');
 });
 
-//login
+// login
 router.get('/login', function(req, res) {
     res.render('login');
+});
+
+// register (post)
+router.post('/register', function(req, res) {
+    var name = req.body.name;
+    var email = req.body.email;
+    var username = req.body.username;
+    var password = req.body.password;
+    var password2 = req.body.password2;
+    
+    // validation
+    req.checkBody('name', 'Name is required').notEmpty();
 });
 
 module.exports = router;
