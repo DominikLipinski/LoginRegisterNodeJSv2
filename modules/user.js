5a448c9b4ebe69fbd0b8263752f4ed7a912d138f
Dominik Lipinski <maboonga@gmail.com>
2017-11-16 22:34:33 +0100

    module exports comparePassword

--- modules/user.js
+++ modules/user.js
@@ -27,3 +27,15 @@ module.exports.createUser = function(newUser, callback){
         });
     });
 }
+
+module.exports.getUserByUsername = function(username, callback) {
+    var query = {username: username};
+    User.findOne(query, callback);
+}
+
+module.exports.comparePassword = function(candidatePasword, hash, callback) {
+    bcrypt.compare(candidatePasword, hash, function(err, isMatch) {
+        if(err) throw err;
+        callback(null, isMatch);
+    });
+}
