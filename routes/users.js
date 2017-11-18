a54204f86984b9d5afd2a642721122307939a23a
Dominik Lipinski <maboonga@gmail.com>
2017-11-18 09:58:48 +0100

    logging works

--- routes/users.js
+++ routes/users.js
@@ -83,7 +83,7 @@ passport.deserializeUser(function(id, done) {
         });
       });
 
-app.post('/login',
+router.post('/login',
 passport.authenticate('local', {successRedirect:'/', failureRedirect:'/users/login',
                     failureFlash: true}),
 function(req, res) {
